ead1598d32cc22924ca7a6d85e5e8eb817819147
eslam <eslamfgazy600@gmail.com>
2022-11-02 01:51:35 +0200

    swich button

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
-import { View, Text } from "react-native";
+import {View, Text, SafeAreaView} from "react-native";
+import Home from "./screens/Home";
+import HeaderTabs from "./components/HeaderTabs";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+    return (
+        <View style={{marginTop:30}}>
+            <HeaderTabs/>
+        </View>
+    );
 }
+
---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {SafeAreaView, Text} from "react-native";
+
+class Home extends Component {
+    render() {
+        return (
+            <SafeAreaView>
+                <Text> djbvkhbvkh</Text>
+            </SafeAreaView>
+        );
+    }
+}
+
+export default Home;
+
